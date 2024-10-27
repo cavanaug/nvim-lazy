8cfb18a50f06f1d4c1d93d6b5458582136ba69a2
John Cavanaugh <59479+cavanaug@users.noreply.github.com>
2024-10-27 07:16:34 -0700

    Alignment

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,3 @@
--- Autocmds are automatically loaded on the VeryLazy event
--- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
+-- Autocmds are automatically loaded after plugins
+-- FYI: autocmds from lazyvim https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -45,5 +45,13 @@ return {
       })
     end,
   },
-  { "andersevenrud/cmp-tmux" },
+  {
+    "hrsh7th/nvim-cmp",
+    optional = true,
+    dependencies = { "andersevenrud/cmp-tmux", lazy = true },
+    opts = function(_, opts)
+      opts.sources = opts.sources or {}
+      table.insert(opts.sources, { name = "tmux" })
+    end,
+  },
 }
