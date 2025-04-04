ba2f374f06e435a4ad50121648656e85b6f4fd9a
John Cavanaugh <59479+cavanaug@users.noreply.github.com>
2025-04-04 05:41:12 -0700

    Markdown and ai fixes

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -1,3 +1,17 @@
+-- Place this in a markdown.lua (or relevant file for Markdown)
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "markdown",
+  callback = function()
+    -- Disable concealment for the entire filetype (markdown)
+    vim.opt.conceallevel = 0
+
+    -- Force Tree-sitter to not conceal HTML comments
+    vim.treesitter.query.set("markdown", "html", [[ (comment) @comment ]])
+
+    -- Optional: Set a visible highlight for HTML comments
+    vim.api.nvim_set_hl(0, "@comment.html", { fg = "#888888", bold = true })
+  end,
+})
 local HOME = os.getenv("HOME")
 return {
   "mfussenegger/nvim-lint",
---
+++ lua/plugins/temporary.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    "saghen/blink.cmp",
+    optional = true,
+    ---@module 'blink.cmp'
+    ---@type blink.cmp.Config
+    opts = {
+      sources = {
+        providers = {
+          path = {
+            enabled = function()
+              return vim.bo.filetype ~= "copilot-chat"
+            end,
+          },
+        },
+      },
+    },
+  },
+}
