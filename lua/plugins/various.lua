b11ad16f706632ca66e334dc81e60855c329b9e8
John Cavanaugh <59479+cavanaug@users.noreply.github.com>
2025-04-05 04:37:50 -0700

    Fixing better escape

---
+++ lua/plugins/various.lua
@@ -0,0 +1,28 @@
+return {
+  { "AndrewRadev/bufferize.vim" }, -- Most items in lazyvim already do this, but still nice to have
+  {
+    "max397574/better-escape.nvim",
+    config = function()
+      require("better_escape").setup({
+        mappings = {
+          -- i for insert, other modes are the first letter too
+          i = {
+            -- map kj to exit insert mode
+            k = { j = "<Esc>" },
+          },
+        },
+      })
+    end,
+  },
+  -- {
+  --   -- Prettier indent markers
+  --   "lukas-reineke/indent-blankline.nvim",
+  --   enabled = false, -- Enable this plugin by default
+  --   opts = function(_, opts)
+  --     opts.indent = { char = "┆" }
+  --     -- opts.indent = { char = "" }
+  --     -- opts.indent = { char = "╎" }
+  --     -- opts.indent = { char = "┃" }
+  --   end,
+  -- },
+}
