0fc1124c5b9532c56c76bc8ec9626e5aa424c93f
John Cavanaugh <59479+cavanaug@users.noreply.github.com>
2025-08-30 02:29:11 -0700

    feat: ✨ add diffview.nvim plugin configuration
    add(lua/plugins/diffview.lua): ➕ Add diffview.nvim setup with custom merge layouts, global diff keymaps, and autocmds including auto-close for empty views

---
+++ lua/plugins/diffview.lua
@@ -0,0 +1,69 @@
+return {
+  {
+    "sindrets/diffview.nvim",
+    config = function()
+      require("diffview").setup({
+        view = {
+          merge_tool = {
+            layout = "diff3_mixed",
+          },
+        },
+        default_args = {
+          DiffviewOpen = { "--untracked-files=false" },
+        },
+      })
+
+      -- Set up diffview keymaps that work in any diff context
+      local function setup_diff_keymaps()
+        if vim.wo.diff or vim.o.foldmethod == "diff" then
+          vim.keymap.set("n", "<C-n>", "]c", { buffer = true, desc = "Next change" })
+          vim.keymap.set("n", "<C-p>", "[c", { buffer = true, desc = "Previous change" })
+          vim.keymap.set("n", "<leader>c3", function()
+            local lib = require("diffview.lib")
+            local Diff3Mixed = require("diffview.scene.layouts.diff_3_mixed").Diff3Mixed
+            local view = lib.get_current_view()
+
+            if view and view.cur_entry then
+              view.cur_entry:convert_layout(Diff3Mixed)
+              view:set_file(view.cur_entry, false)
+            end
+          end, { buffer = true, desc = "Switch to diff3_mixed layout" })
+          vim.keymap.set("n", "<leader>c4", function()
+            local lib = require("diffview.lib")
+            local Diff4Mixed = require("diffview.scene.layouts.diff_4_mixed").Diff4Mixed
+            local view = lib.get_current_view()
+
+            if view and view.cur_entry then
+              view.cur_entry:convert_layout(Diff4Mixed)
+              view:set_file(view.cur_entry, false)
+            end
+          end, { buffer = true, desc = "Switch to diff4_mixed layout" })
+        end
+      end
+
+      -- Apply keymaps when entering buffers
+      vim.api.nvim_create_autocmd({ "BufEnter", "WinEnter" }, {
+        callback = setup_diff_keymaps,
+      })
+
+      -- Auto-close if no merge conflicts found
+      vim.api.nvim_create_autocmd("User", {
+        pattern = "DiffviewViewOpened",
+        callback = function()
+          local lib = require("diffview.lib")
+          local view = lib.get_current_view()
+
+          if view and view.panel and view.panel.list_view then
+            local files = view.panel.list_view:get_items()
+            if #files == 0 then
+              vim.schedule(function()
+                vim.cmd("DiffviewClose")
+                print("No merge conflicts found - closing diffview")
+              end)
+            end
+          end
+        end,
+      })
+    end,
+  },
+}
