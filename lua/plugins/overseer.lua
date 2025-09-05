45735f39565f2d3b05361db3e08bb70a3f5e222b
John Cavanaugh <59479+cavanaug@users.noreply.github.com>
2025-09-05 01:42:59 -0700

    config: ⚙️ enhance overseer.nvim keymaps and taskfile detection
    config(lua/plugins/overseer.lua): ⚙️ disable default <leader>o mappings and add new <leader>O shortcuts; extend Taskfile search to include lowercase variants by overriding cache_key and condition; register which-key groups for overseer commands

--- lua/plugins/overseer.lua
+++ lua/plugins/overseer.lua
@@ -1,27 +1,73 @@
 return {
-  "stevearc/overseer.nvim",
-  opts = function(_, opts)
-    -- Override taskfiles to include case variants
-    local task_provider = require("overseer.template.task")
-    local original_taskfiles = {
-      "Taskfile.yml",
-      "Taskfile.yaml", 
-      "Taskfile.dist.yml",
-      "Taskfile.dist.yaml",
-    }
-    
-    -- Add lowercase variants
-    local taskfiles = vim.list_extend(original_taskfiles, {
-      "taskfile.yml",
-      "taskfile.yaml",
-    })
-    
-    -- Override the find_taskfile function
-    local original_find_taskfile = task_provider.cache_key
-    task_provider.cache_key = function(search_opts)
-      return vim.fs.find(taskfiles, { upward = true, type = "file", path = search_opts.dir })[1]
-    end
-    
-    return opts
-  end,
+  {
+    "stevearc/overseer.nvim",
+    keys = {
+      -- Disable default <leader>o keymaps
+      { "<leader>ow", false },
+      { "<leader>oo", false },
+      { "<leader>oq", false },
+      { "<leader>oi", false },
+      { "<leader>ob", false },
+      { "<leader>ot", false },
+      { "<leader>oc", false },
+      
+      -- New <leader>O keymaps
+      { "<leader>Ow", "<cmd>OverseerToggle<cr>",      desc = "Task list" },
+      { "<leader>Oo", "<cmd>OverseerRun<cr>",         desc = "Run task" },
+      { "<leader>Oq", "<cmd>OverseerQuickAction<cr>", desc = "Action recent task" },
+      { "<leader>Oi", "<cmd>OverseerInfo<cr>",        desc = "Overseer Info" },
+      { "<leader>Ob", "<cmd>OverseerBuild<cr>",       desc = "Task builder" },
+      { "<leader>Ot", "<cmd>OverseerTaskAction<cr>",  desc = "Task action" },
+      { "<leader>Oc", "<cmd>OverseerClearCache<cr>",  desc = "Clear cache" },
+    },
+    config = function(_, opts)
+      -- Extend the taskfiles list to include lowercase variants
+      local task_template = require("overseer.template.task")
+      
+      -- Override the taskfiles table directly
+      local original_taskfiles = {
+        "Taskfile.yml",
+        "Taskfile.yaml",
+        "Taskfile.dist.yml",
+        "Taskfile.dist.yaml",
+      }
+      
+      -- Add lowercase variants
+      local extended_taskfiles = vim.list_extend(vim.deepcopy(original_taskfiles), {
+        "taskfile.yml",
+        "taskfile.yaml",
+      })
+      
+      -- Replace the find_taskfile function in the cache_key
+      task_template.cache_key = function(opts)
+        return vim.fs.find(extended_taskfiles, { upward = true, type = "file", path = opts.dir })[1]
+      end
+      
+      -- Also update the condition callback to use the extended list
+      local original_condition = task_template.condition.callback
+      task_template.condition.callback = function(opts)
+        if vim.fn.executable("task") == 0 then
+          return false, 'Command "task" not found'
+        end
+        local taskfile = vim.fs.find(extended_taskfiles, { upward = true, type = "file", path = opts.dir })[1]
+        if not taskfile then
+          return false, "No Taskfile found"
+        end
+        return true
+      end
+      
+      -- Setup overseer with default config
+      require("overseer").setup(opts)
+    end,
+  },
+  {
+    "folke/which-key.nvim",
+    optional = true,
+    opts = {
+      spec = {
+        { "<leader>o", group = "overseer", mode = "n" },
+        { "<leader>O", group = "overseer", mode = "n" },
+      },
+    },
+  },
 }
