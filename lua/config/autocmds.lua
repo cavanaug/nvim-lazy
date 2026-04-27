bd0b7939795dd5daf93df304761c86d336b5efcb
John Cavanaugh <59479+cavanaug@users.noreply.github.com>
2026-04-27 07:20:14 -0700

    fix: 🐛 remove red SpellBad foreground
    fix(lua/config/autocmds.lua): 🐛 remove hardcoded red fg from SpellBad so colorschemes control color

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -39,11 +39,13 @@
 vim.api.nvim_create_autocmd("ColorScheme", {
   group = vim.api.nvim_create_augroup("user_spell_highlights", { clear = true }),
   callback = function()
-    vim.api.nvim_set_hl(0, "SpellBad", { fg = "#ff0000", italic = true, underline = true })
+    -- vim.api.nvim_set_hl(0, "SpellBad", { fg = "#ff0000", italic = true, underline = true })
+    vim.api.nvim_set_hl(0, "SpellBad", { italic = true, underline = true })
   end,
 })
 -- Apply immediately for the current colorscheme
-vim.api.nvim_set_hl(0, "SpellBad", { fg = "#ff0000", italic = true, underline = true })
+-- vim.api.nvim_set_hl(0, "SpellBad", { fg = "#ff0000", italic = true, underline = true })
+vim.api.nvim_set_hl(0, "SpellBad", { italic = true, underline = true })
 
 vim.api.nvim_create_augroup("user_filetype_settings", { clear = true })
 
